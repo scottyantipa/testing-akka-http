6a56023313201a095e395fb20f833d2c764d9ab6
Scott Antipa <scottantipa@gmail.com>
2018-01-26 16:09:26 -0800

    Working project showing basic akka-http. Bootstrapped from their QuickStart template.

---
+++ src/main/scala/com/lightbend/akka/http/sample/QuickstartServer.scala
@@ -0,0 +1,51 @@
+package com.lightbend.akka.http.sample
+
+import scala.concurrent.{ Await, ExecutionContext, Future }
+import scala.concurrent.duration.Duration
+
+import akka.actor.{ ActorRef, ActorSystem }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+
+//#main-class
+object QuickstartServer extends App with UserRoutes {
+
+  // set up ActorSystem and other dependencies here
+  //#main-class
+  //#server-bootstrapping
+  implicit val system: ActorSystem = ActorSystem("helloAkkaHttpServer")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  //#server-bootstrapping
+
+  // Needed for the Future and its methods flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContext = system.dispatcher
+
+  val userRegistryActor: ActorRef = system.actorOf(UserRegistryActor.props, "userRegistryActor")
+
+  //#main-class
+  // from the UserRoutes trait
+  lazy val routes: Route = userRoutes
+  //#main-class
+
+  //#http-server
+  val serverBindingFuture: Future[ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/")
+
+  // Commenting out because this block terminates the server prematurely
+  //
+  //  serverBindingFuture
+  //    .flatMap(_.unbind())
+  //    .onComplete { done =>
+  //      println("within onComplete")
+  //      done.failed.map { ex => log.error(ex, "Failed unbinding") }
+  //      system.terminate()
+  //    }
+
+  Await.result(system.whenTerminated, Duration.Inf)
+  //#http-server
+  //#main-class
+}
+//#main-class
